ad1068c6ae5c8e58dfad4e7216f5ae94c0473ff9
ReshmaGubbala <reshma_csd@srkrec.edu.in>
2025-07-25 20:08:10 +0530

    Delete mlpr directory

--- mlpr/translate.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import translate_v2 as translate
-
-def translate_text_google(text, target_lang='en'):
-    client = translate.Client()
-    result = client.translate(text, target_language=target_lang)
-    return result['translatedText']
